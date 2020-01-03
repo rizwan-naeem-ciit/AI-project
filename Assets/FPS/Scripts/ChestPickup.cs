6e1676a8ebccba6d18c2e2e7a32c29605bb1e446
rizwan malik <rizwan.naeem.ciit@gmail.com>
2020-01-03 22:55:34 +0500

    second commit

---
+++ Assets/FPS/Scripts/ChestPickup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ChestPickup: MonoBehaviour
+{
+    [Header("Parameters")]
+    [Tooltip("Amount of health to heal on pickup")]
+    public bool key = false;
+
+    Pickup m_Pickup;
+
+    void Start()
+    {
+        m_Pickup = GetComponent<Pickup>();
+        DebugUtility.HandleErrorIfNullGetComponent<Pickup, HealthPickup>(m_Pickup, this, gameObject);
+
+        // Subscribe to pickup action
+        m_Pickup.onPick += OnPicked;
+    }
+
+    void OnPicked(PlayerCharacterController player)
+    {
+
+        Health playerHealth = player.GetComponent<Health>();
+        if (playerHealth && player.key)
+        {
+
+
+            m_Pickup.PlayPickupFeedback();
+
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ Assets/FPS/Scripts/keypickup.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+using UnityEngine;
+
+public class keypickup : MonoBehaviour
+{
+    [Header("Parameters")]
+    [Tooltip("Amount of health to heal on pickup")]
+   
+
+    Pickup m_Pickup;
+
+    void Start()
+    {
+        m_Pickup = GetComponent<Pickup>();
+        DebugUtility.HandleErrorIfNullGetComponent<Pickup, HealthPickup>(m_Pickup, this, gameObject);
+
+        // Subscribe to pickup action
+        m_Pickup.onPick += OnPicked;
+    }
+
+    void OnPicked(PlayerCharacterController player)
+    {
+
+        Health playerHealth = player.GetComponent<Health>();
+        if (playerHealth)
+        {
+            player.key = true;
+
+            m_Pickup.PlayPickupFeedback();
+
+            Destroy(gameObject);
+        }
+    }
+}
+
